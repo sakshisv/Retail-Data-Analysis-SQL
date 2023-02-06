837d7389ea70e065ac0544126087ddc6fb14fcff
Sakshi <sakshi0317sv@gmail.com>
2023-02-06 23:25:58 +0530

    Third Commit

--- Retail Data Analysis- SQL2.sql
+++ Retail Data Analysis- SQL2.sql
@@ -6,3 +6,14 @@ select * from Customer
 select * from prod_cat_info
 select * from Transactions
 
+
+--DATA PREPARATION AND UNDERSTANDING
+
+--Q1. What is the total number of rows in each of the 3 tables in the database?
+
+select count(*) No_of_Rows from Customer
+Union
+select count(*) from prod_cat_info
+Union
+select count(*) from Transactions
+
